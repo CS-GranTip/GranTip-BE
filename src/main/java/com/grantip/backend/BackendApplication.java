29f6b2e58136604738cffe4b9da16338cd99687d
장윤정 <wkddbs3935@naver.com>
2025-06-29 17:37:47 +0900

    fix: @EnableJpaAuditing 설정

--- src/main/java/com/grantip/backend/BackendApplication.java
+++ src/main/java/com/grantip/backend/BackendApplication.java
@@ -2,10 +2,10 @@ package com.grantip.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaAuditing
 public class BackendApplication {
 
   public static void main(String[] args) {
