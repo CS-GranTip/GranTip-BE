d3b33c46987390b43e0d54463edb86543d97be72
장윤정 <wkddbs3935@naver.com>
2025-07-29 00:49:26 +0900

    refactor: UnivYear 필드값 변경 및 JsonCreator 정의

--- src/main/java/com/grantip/backend/domain/user/domain/constant/UnivYear.java
+++ src/main/java/com/grantip/backend/domain/user/domain/constant/UnivYear.java
@@ -1,26 +1,38 @@
 package com.grantip.backend.domain.user.domain.constant;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.grantip.backend.global.code.ErrorCode;
+import com.grantip.backend.global.exception.CustomException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum UnivYear {
-    FIRST_SEMESTER("대학1학기"),
-    SECOND_SEMESTER("대학2학기"),
-    THIRD_SEMESTER("대학3학기"),
-    FOURTH_SEMESTER("대학4학기"),
-    FIFTH_SEMESTER("대학5학기"),
-    SIXTH_SEMESTER("대학6학기"),
-    SEVENTH_SEMESTER("대학7학기"),
-    EIGHTH_SEMESTER_OR_ABOVE("대학8학기 이상");
+  FRESHMAN("대학신입생"),
+  SECOND_SEMESTER("대학2학기"),
+  THIRD_SEMESTER("대학3학기"),
+  FOURTH_SEMESTER("대학4학기"),
+  FIFTH_SEMESTER("대학5학기"),
+  SIXTH_SEMESTER("대학6학기"),
+  SEVENTH_SEMESTER("대학7학기"),
+  EIGHTH_SEMESTER_OR_ABOVE("대학8학기이상");
 
-    // SEMESTER_1, SEM_1 이런식으로 축약 가능
+  private final String description;
 
-    private final String description;
+  @JsonValue
+  public String getDescription() {
+    return description;
+  }
 
-    UnivYear(String description) {
-        this.description = description;
+  @JsonCreator
+  public static UnivYear fromValue(String value) {
+    for (UnivYear year : UnivYear.values()) {
+      if (year.name().equalsIgnoreCase(value)) {
+        return year;
+      }
     }
-    @JsonValue
-    public String getDescription() {
-        return description;
-    }
+    throw new CustomException(ErrorCode.INVALID_UNIV_YEAR);
+  }
 }
