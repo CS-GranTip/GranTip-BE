9b015735aabf7c999968e9c79ef8390a0c1a5456
20011744 <141496725+cksdid202@users.noreply.github.com>
2025-07-28 18:52:00 +0900

    feat: JavaMailSender bean 정의를 위한 EmailConfig 추가

---
+++ src/main/java/com/grantip/backend/global/config/EmailConfig.java
@@ -0,0 +1,30 @@
+package com.grantip.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.naver.com"); // SMTP 서버 설정
+        mailSender.setPort(587);
+        mailSender.setUsername("cisdid202@naver.com"); // 이메일 사용자
+        mailSender.setPassword("YX6ZFVXPV2U8"); // 이메일 비밀번호
+
+        Properties properties = mailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "smtp");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
+
