39af0eac43c62ebf3f1b3e18a85ec7d7d5cab4bd
장윤정 <wkddbs3935@naver.com>
2025-07-31 07:16:44 +0900

    feat: 1차 db 필터링 로직 구현

--- src/main/java/com/grantip/backend/domain/scholarship/repository/ScholarshipRepositoryImpl.java
+++ src/main/java/com/grantip/backend/domain/scholarship/repository/ScholarshipRepositoryImpl.java
@@ -5,9 +5,15 @@ import com.grantip.backend.domain.scholarship.domain.dto.response.ScholarshipSum
 import com.grantip.backend.domain.region.domain.entity.QRegion;
 import com.grantip.backend.domain.scholarship.domain.entity.QScholarship;
 import com.grantip.backend.domain.scholarship.domain.entity.QScholarshipRegion;
+import com.grantip.backend.domain.scholarship.domain.entity.Scholarship;
+import com.grantip.backend.domain.user.domain.entity.User;
+import com.grantip.backend.domain.user.domain.entity.UserExtraInfo;
 import com.grantip.backend.global.util.database.QueryDslUtil;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
+import java.util.List;
 import org.springframework.util.StringUtils;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -63,7 +69,7 @@ public class ScholarshipRepositoryImpl implements ScholarshipRepositoryCustom {
         .leftJoin(region).on(scholarshipRegion.region.eq(region))
         .where(combinedWhereClause)
         .distinct()
-        .orderBy(scholarship.applicationStartDate.desc());
+        .orderBy(scholarship.applicationEndDate.desc());
 
     // 카운트 쿼리
     JPAQuery<Long> countQuery = queryFactory
@@ -74,5 +80,51 @@ public class ScholarshipRepositoryImpl implements ScholarshipRepositoryCustom {
         .where(combinedWhereClause);
 
     return QueryDslUtil.fetchPage(contentQuery, countQuery, pageable);
+  }
+
+  public List<Scholarship> findFilteredScholarships(User user, UserExtraInfo userInfo){
+    // 신청 기간 필터링 - 추후 데이터 많아지면 활성화
+    /*LocalDate today = LocalDate.now();
+    builder.and(scholarship.applicationStartDate.loe(today))
+        .and(scholarship.applicationEndDate.goe(today));*/
+
+    // 대학 구분 필터링 - 장학금에 대학구분 조건이 없거나 사용자와 일치
+    BooleanExpression universityCondition = scholarship.universityCategories.isEmpty();
+    if(user.getUniversityCategory() != null){
+      universityCondition = universityCondition.or(scholarship.universityCategories.contains(user.getUniversityCategory()));
+    }
+
+    // 거주 지역 필터링 장학금에 지역 조건이 없거나 사용자 주소와 일치
+    BooleanExpression regionCondition = JPAExpressions
+        .selectFrom(scholarshipRegion)
+        .where(scholarshipRegion.scholarship.eq(scholarship))
+        .notExists();
+
+    if(user.getAddress() != null || user.getResidentAddress() != null){
+      BooleanBuilder userRegionMatch = new BooleanBuilder();
+      if(user.getAddress() != null){
+        userRegionMatch.or(scholarshipRegion.region.eq(user.getAddress()));
+      }
+      if(user.getResidentAddress() != null){
+        userRegionMatch.or(scholarshipRegion.region.eq(user.getResidentAddress()));
+      }
+      regionCondition = regionCondition.or(
+          JPAExpressions
+              .selectFrom(scholarshipRegion)
+              .where(scholarshipRegion.scholarship.eq(scholarship).and(userRegionMatch))
+              .exists()
+      );
+    }
+
+    return queryFactory
+        .select(scholarship)
+        .distinct()
+        .from(scholarship)
+        // 기준 테이블들 Fetch Join
+        .leftJoin(scholarship.gradeCriteria).fetchJoin()
+        .leftJoin(scholarship.incomeCriteria).fetchJoin()
+        .leftJoin(scholarship.generalCriteria).fetchJoin()
+        .where(universityCondition, regionCondition)
+        .fetch();
   }
 }
