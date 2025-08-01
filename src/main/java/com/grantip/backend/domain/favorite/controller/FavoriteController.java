ec05c99d34e123e3c53c3b44e75c08048df4b53a
장윤정 <wkddbs3935@naver.com>
2025-08-01 21:24:15 +0900

    feat: 좋아요 등록/취소 및 좋아요 목록 조회 api 구현

--- src/main/java/com/grantip/backend/domain/favorite/controller/FavoriteController.java
+++ src/main/java/com/grantip/backend/domain/favorite/controller/FavoriteController.java
@@ -1,11 +1,69 @@
 package com.grantip.backend.domain.favorite.controller;
 
+import com.grantip.backend.domain.favorite.domain.dto.request.FavoriteScholarshipRequest;
 import com.grantip.backend.domain.favorite.service.FavoriteService;
+import com.grantip.backend.domain.scholarship.domain.dto.response.ScholarshipSummaryResponse;
+import com.grantip.backend.domain.scholarship.service.ScholarshipService;
+import com.grantip.backend.domain.user.service.UserService;
+import com.grantip.backend.global.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/favorites")
 public class FavoriteController {
-    private final FavoriteService favoriteService;
+  private final FavoriteService favoriteService;
+  private final ScholarshipService scholarshipService;
+  private final UserService userService;
+
+  // 좋아요 등록 or 취소
+  @PostMapping("/{scholarshipId}")
+  public ResponseEntity<ApiResponse<Boolean>> toggleFavorite(
+      @AuthenticationPrincipal UserDetails userDetails,
+      @PathVariable Long scholarshipId){
+
+    boolean isFavorited = favoriteService.toggleFavorite(
+        scholarshipService.findById(scholarshipId),
+        userService.findByEmail(userDetails.getUsername())
+    );
+
+    String message = isFavorited ? "좋아요 등록에 성공했습니다." : "좋아요 취소에 성공했습니다.";
+
+    return ResponseEntity.ok(
+        ApiResponse.
+            <Boolean>builder()
+            .success(true)
+            .code(200)
+            .result(isFavorited)
+            .message(message)
+            .build()
+      );
+    }
+
+  // 사용자의 좋아요 목록 조회
+  @GetMapping
+  public ResponseEntity<ApiResponse<Page<ScholarshipSummaryResponse>>> getFavoritedScholarships(
+      @AuthenticationPrincipal UserDetails userDetails,
+      @ParameterObject @Valid FavoriteScholarshipRequest request){
+    return ResponseEntity.ok(
+      ApiResponse
+          .<Page<ScholarshipSummaryResponse>>builder()
+          .success(true)
+          .code(200)
+          .result(favoriteService.findFavoritedScholarships(userService.findByEmail(userDetails.getUsername()), request.toPageable()))
+          .message("좋아요한 장학금 목록 조회에 성공했습니다.")
+          .build()
+    );
+  }
 }
--- src/main/java/com/grantip/backend/domain/favorite/service/FavoriteService.java
+++ src/main/java/com/grantip/backend/domain/favorite/service/FavoriteService.java
@@ -1,11 +1,57 @@
 package com.grantip.backend.domain.favorite.service;
 
+import com.grantip.backend.domain.favorite.domain.entity.FavoriteScholar;
 import com.grantip.backend.domain.favorite.repository.FavoriteRepository;
+import com.grantip.backend.domain.scholarship.domain.dto.response.ScholarshipSummaryResponse;
+import com.grantip.backend.domain.scholarship.domain.entity.Scholarship;
+import com.grantip.backend.domain.scholarship.mapper.ScholarshipMapper;
+import com.grantip.backend.domain.user.domain.entity.User;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FavoriteService {
-    private final FavoriteRepository favoriteRepository;
+  private final FavoriteRepository favoriteRepository;
+  private final ScholarshipMapper scholarshipMapper;
+
+  /**
+   * 장학금 좋아요 등록 또는 취소
+   *
+   * @param scholarship 장학금
+   * @param user  사용자
+   * @return 좋아요 등록/취소 후의 상태 (등록: true, 취소: false)
+   */
+  public boolean toggleFavorite(Scholarship scholarship, User user){
+    Optional<FavoriteScholar> existingFavorite = favoriteRepository.findByScholarshipAndUser(scholarship, user);
+
+    if(existingFavorite.isPresent()){
+      favoriteRepository.delete(existingFavorite.get());
+      return false;
+    }
+    else{
+      favoriteRepository.save(FavoriteScholar.builder()
+          .scholarship(scholarship)
+          .user(user)
+          .build()
+      );
+      return true;
+    }
+  }
+
+  /**
+   * 장학금 좋아요 목록 조회
+   *
+   * @param user  사용자
+   * @param pageable pageable
+   * @return 좋아요한 리스트 페이지
+   */
+  public Page<ScholarshipSummaryResponse> findFavoritedScholarships(User user, Pageable pageable){
+    return favoriteRepository.findByUser(user, pageable)
+        .map(FavoriteScholar::getScholarship)
+        .map(scholarshipMapper::toSummaryResponse);
+  }
 }
