f53ef02113ecbea597d53e305d917337181f57b0
장윤정 <wkddbs3935@naver.com>
2025-08-01 21:23:27 +0900

    feat: 좋아요한 장학금 요청 dto 정의

---
+++ src/main/java/com/grantip/backend/domain/favorite/domain/dto/request/FavoriteScholarshipRequest.java
@@ -0,0 +1,31 @@
+package com.grantip.backend.domain.favorite.domain.dto.request;
+
+import com.grantip.backend.global.constant.PageableConstant;
+import com.grantip.backend.global.util.database.PageableUtil;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Builder.Default;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+
+public class FavoriteScholarshipRequest {
+  @Default
+  @Min(value = 1, message = "페이지 번호는 1 이상이어야 합니다.")
+  @Max(value = Integer.MAX_VALUE, message = "정수 최대 범위를 넘을 수 없습니다.")
+  private Integer pageNumber = 1;
+
+  @Default
+  @Min(value = 1, message = "페이지 크기는 1 이상이어야 합니다.")
+  @Max(value = PageableConstant.MAX_PAGE_SIZE, message = "페이지 당 데이터 최댓값은 " + PageableConstant.MAX_PAGE_SIZE + "개 입니다.")
+  private Integer pageSize = 10;
+
+  public Pageable toPageable(){
+    return PageableUtil.createPageable(
+        pageNumber,
+        pageSize,
+        PageableConstant.DEFAULT_PAGE_SIZE,
+        "createdAt",
+        Direction.DESC
+    );
+  }
+}
