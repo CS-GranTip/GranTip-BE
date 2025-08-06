7ab2b494ef051698afeb849c802837dec5e9bb66
장윤정 <wkddbs3935@naver.com>
2025-08-06 21:45:43 +0900

    feat: redis config 설정 수정

--- src/main/java/com/grantip/backend/global/config/RedisConfig.java
+++ src/main/java/com/grantip/backend/global/config/RedisConfig.java
@@ -1,14 +1,20 @@
 package com.grantip.backend.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -47,12 +53,28 @@ public class RedisConfig {
         return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(LettuceConnectionFactory factory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        return template;
-    }
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory, ObjectMapper objectMapper) {
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(factory);
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+    return template;
+  }
+
+  /**
+   * @Cacheable을 위한 CacheManager 설정
+   */
+  @Bean
+  public CacheManager cacheManager(LettuceConnectionFactory factory, ObjectMapper objectMapper) {
+    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
+        .entryTtl(Duration.ofDays(1));
+
+    return RedisCacheManager.RedisCacheManagerBuilder
+        .fromConnectionFactory(factory)
+        .cacheDefaults(redisCacheConfiguration)
+        .build();
+  }
 }
